cde5965b2da4501f3b36af14ae2b080c72cf2f7c
Andrii <andryyo@ukr.net>
2022-09-22 16:35:18 +0300

    Switched to ag grid

--- src/ColorTable.js
+++ src/ColorTable.js
@@ -1,17 +1,21 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import * as chromatism from "chromatism"
-import { DataGrid } from "@mui/x-data-grid";
-import { data } from "VallejoGame"
+import { data as vallejoGame } from "VallejoGame"
+import { AgGridReact } from "ag-grid-react";
+
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
 
 const ColorTable = props => {
-    const colors = React.useMemo(() => data.split('\n').map(s => {
+    const colors = React.useMemo(() => vallejoGame.split('\n').map(s => {
         const code = s.substring(s.indexOf("#"));
         const color = chromatism.convert(code).hsv;
         const H = color.h;
         const S = color.s || 0;
         const V = color.v;
         return {
+          collection: "Vallejo Game Color",
           name: s.substring(0, s.indexOf("#") - 1),
           id: code,
           color: color,
@@ -21,15 +25,30 @@ const ColorTable = props => {
         };
     }), []);
 
+    const colorsWithDelta = React.useMemo(() => {
+      return colors.map(c => {
+        const delta = props.selectedColor ? chromatism.difference(c.color, props.selectedColor, 2, 1) : null;
+        return {...c, delta};
+      })}, [props.selectedColor]);
+
     const columns = React.useMemo(() => [{
+      field: "collection",
+      headerName: "Collection",
+      width: 150,
+      filter: true
+    }, {
       field: "name",
       headerName: "Name",
-      width: 150
+      width: 150,
+      sortable: true,
+      filter: true
     }, {
       field: "id",
       headerName: "Code",
       type: "number",
       width: 100,
+      sortable: true,
+      filter: true,
       renderCell: props => {
         return (<div style={{backgroundColor: props.value}} className="ColorCell">{props.value}</div>)
       }
@@ -37,38 +56,35 @@ const ColorTable = props => {
       field: "H",
       headerName: "Hue",
       type: "number",
-      width: 100
+      width: 100,
+      sortable: true,
+      filter: true,
     }, {
       field: "S",
       headerName: "Saturation",
       type: "number",
-      width: 100
+      width: 100,
+      sortable: true,
+      filter: true
     }, {
       field: "V",
       headerName: "Value",
       type: "number",
-      width: 100
+      width: 100,
+      sortable: true,
+      filter: true
     }, {
       field: "delta",
       headerName: "Delta",
       type: "number",
       width: 100,
-      valueGetter: param => {
-        return props.selectedColor ? chromatism.difference(param.row.color, props.selectedColor, 2, 1) : null;
-      }
-    }], [props.selectedColor]);
+      sortable: true,
+      filter: true
+    }], []);
 
-    React.useEffect(() => {
-      setSortModel([ { field: "delta", sort: "asc" } ]);
-    }, [props.selectedColor])
-
-    const [sortModel, setSortModel] = React.useState([ { field: "delta", sort: "asc" } ]);
-
-    return (<DataGrid
-        rows={colors}
-        columns={columns}
-        sortModel={sortModel}
-        onSortModelChange={(e) => setSortModel(e)}
+    return (<AgGridReact className="ag-theme-alpine"
+        rowData={colorsWithDelta}
+        columnDefs={columns}
     />)
   };
 
