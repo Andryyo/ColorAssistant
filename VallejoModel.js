684e638500793af18f543199e079c26ec30bb1e0
Andrii <andryyo@ukr.net>
2022-10-05 21:25:28 +0300

    Updates

---
+++ VallejoModel.js
@@ -0,0 +1,170 @@
+const vallejoModel = `Sunset Red #c22c5f
+Brown Rose #cf8774
+Beige Red #c79166
+German Orange #f05426
+German Yellow #e8e589
+Oxford Blue #3a465f
+Blue Green #1a9496
+Royal Blue #41558d
+Royal Purple #4b2967
+Blue Violet #795c9a
+Violet Red #6b2946
+Burnt Cadmium Red #623535
+Basic Skintone #fbbc8c
+Luftwaffe Uniform WW2 #3d474f
+Scarlet #da3630
+Red Leather #93453d
+Iraqi Sand #b89972
+Offwhite #efeee2
+German Cam. Beige WW2 #9c8d70
+German Cam. Black Brown #5b3528
+Luftwaffe Cam. Green #596b3f
+German Cam. Black Brown #553528
+German Cam. Orange Ochre #9d7e55
+German Cam. Pale Brown #82685e
+German Cam. Medium Brown #722b2f
+Lime Green #6fae4e
+Amaranth Red #cc4338
+German Field Grey WW2 #595748
+German Cam. Bright Green #528944
+Salmon Rose #f89d87
+London Grey #4a555e
+Pale Sand #d1c49c
+Emerald #00906a
+Ultramarine #3b5da7
+Light Turquoise #0094a5
+Andrea Blue #008dc9
+Cork Brown #a57b63
+Deep Sky Blue #00a6da
+Sunny Skintone #f4b171
+Mahogany Brown #804b47
+Dark Sand #d6b779
+Medium Olive #226b2e
+Bright Orange #f26833
+Ochre Brown #a77237
+Golden Olive #78923b
+Ice Yellow #feef87
+Black Red [Cadmium Brown] #5f2e39
+Medium Fleshtone #c08245
+Glossy Black #010101
+Black Grey #2d373a
+Grey Green #565a60
+Dark Blue Grey #4b5751
+Dark Sea Green #2f4844
+Basalt Grey #576860
+Medium Sea Grey #869393
+Leather Brown #4a4639
+Chocolate Brown #5f5047
+US Field Drab #705d43
+Tan Earth #9c7962
+Beige Brown #78584a
+Brown Sand #b08064
+Gold Brown #ba8636
+Green Brown #8d7b55
+Khaki Grey #8b7f5d
+Yellow Green #91864b
+Middlestone #9c9660
+Silver Grey #d8dfd5
+Stone Grey #959980
+Pastel Green #a4bb99
+Green Grey #827e61
+Brown Violet #4e5d4d
+Olive Grey #39584f
+USA Olive Drab #3b4947
+Reflective Green #496244
+Intermediate Green #49944f
+Yellow Olive #26392e
+US Dark Green #5b6955
+Cam. Olive Green #3c5549
+Gunship Green #3d685e
+German Cam. Extra Dark Green #30504a
+Bronce Green #2a534f
+Dark Sea Blue #2a4048
+Dark Prussian Blue #192c43
+French Mirage Blue #557880
+Pastel Blue #67859b
+Azure #717cad
+Intermediate Blue #607a80
+Dark Blue Grey #5c7476
+Blue Grey Pale #9ea7a5
+Pale Blue #83b5b2
+Pale Grey Blue #bacdd0
+Carmine Red #b73035
+Vermillion #d22b2d
+Orange Red #e34c26
+Light Orange #f68627
+Tan Yellow #b5915d
+Yellow Ochre #c19448
+Green Ochre #a98969
+Deep Yellow #ffe30c
+Sand Yellow #d7bf75
+Beige #dbc58e
+Ivory #eff1d9
+German Uniform #286254
+English Uniform #715833
+Uniform Green #577a38
+Japan Uniform WW2 #998546
+Russian Uniform WW2 #606d53
+Blue #043667
+Red #91323a
+Dark Flesh #d9a262
+Light Flesh #feedd0
+Light Brown #bf764e
+Dark Blue #0d5a86
+Saddle Brown #89584f
+Burnt Umber #665148
+Light Green #00a850
+Grey Blue #45708d
+Old Rose #d47b7d
+Magenta #a92a56
+Dark Red #a24a46
+Dark Sand #d1b47e
+Golden Yellow #f7c65e
+Light Yellow #f7ea62
+Black #252b31
+White #ffffff
+Lemon Yellow #fef10e
+Flat Yellow #ffcd25
+Yellow Green #d9e02a
+Flat Flesh #d8a977
+Clear Orange #f1552c
+Flat Red #aa3037
+Pink #d37497
+Purple #834a6c
+Violet #4b2f65
+Sky Blue #86d6f7
+Flat Blue #336991
+Medium Blue #066588
+Field Blue #436872
+Prussian Blue #2a546e
+Turquoise #008e9c
+Olive Green #6a8b3a
+Flat Green #2a6531
+Park Green Flat #008c5d
+Deep Green #0a6b48
+Grey Grey #aecbbb
+Light Green Blue #6d9a95
+Light Sea Grey #97b4b5
+Green Sky #59ad84
+Military Green #3e5848
+Buff #d2ca9f
+Desert Yellow #b19151
+Dark Yellow #9d9059
+German Cam. Dark Green #2f4845
+Black Green #19423e
+Orange Brown #b86f44
+Cavalry Brown #853f3d
+Flat Earth #87643c
+Flat Brown #764841
+Hull Red #522724
+Deck Tan #bec5b3
+Medium Grey #909b85
+Khaki #969163
+Sky Grey #b3b6b2
+Light Grey #949c9a
+Dark Sea Grey #787d7a
+Neutral Grey #64767a
+White Grey #eef1ec
+Dark Grey #233e3d
+German Grey #192f30
+`;
